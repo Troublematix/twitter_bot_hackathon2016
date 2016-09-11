4b112bd42c512fed057cdf284e9c5e6f20b0ba85
Lan Ly <lanly@uw.edu>
2016-09-11 14:43:20 -0700

    submit

---
+++ twitter_bot.py
@@ -0,0 +1,40 @@
+#!/usr/local/virtualenvs/ds-py3/bin/python
+import json
+import tweepy
+from time import sleep
+
+SECRETS_FILE = "secrets.json"
+CHECK_INTERVAL = 5
+
+
+def main():
+    # Twitter API setup
+    with open(SECRETS_FILE) as f:
+        secrets = json.load(f)
+    auth = tweepy.OAuthHandler(secrets['consumer_key'], secrets['consumer_secret'])
+    auth.set_access_token(secrets['access_token'], secrets['access_secret'])
+    api = tweepy.API(auth)
+
+    while(True):
+        search = api.search("#motivaSparta")
+        for x in range(0, len(search)):
+            try:
+                api.update_status("@{} This is SPARTA, GRRRR!!!!!!!!!!".format(search[x].user.screen_name),
+                                  search[x].user.id)
+            except tweepy.error.TweepError as e:
+                # print(repr(e))
+                next
+        sleep(CHECK_INTERVAL)
+
+    # search = api.search("#xmotivaSparta")
+    # for x in range(0, len(search)):
+    #     try:
+    #         # api.update_status("This is SPARTA, GRRRR!!!!!!!!!!", search[x].id, IMG)
+    #         h = api.media_upload("sparta.jpg")
+    #         print h
+    #     except tweepy.error.TweepError as e:
+    #         print(repr(e))
+    #         next
+
+if __name__ == '__main__':
+    main()
